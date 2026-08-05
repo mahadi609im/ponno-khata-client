4db4335686fc1706e131c233d35802fceabac4e4
mahadi609im <maha609im@gmail.com>
2026-08-05 12:26:29 +0600

    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -129,7 +129,7 @@ const HomePage = () => {
         </div>
       ) : filteredProducts.length > 0 ? (
         viewMode === 'grid' ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {filteredProducts.map(product => (
               <ProductCard key={product._id} product={product} />
             ))}
