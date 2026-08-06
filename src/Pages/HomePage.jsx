4cc707f1f1517129670b5367492885d95b700dce
mahadi609im <maha609im@gmail.com>
2026-08-06 14:04:16 +0600

    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { useOutletContext } from 'react-router';
 import AllProducts from '../Components/All Products/AllProducts';
 import CategoryProducts from '../components/CategoryProducts';
-
 const HomePage = () => {
   // Outlet context থেকে activeCategory রিসিভ করা
   const context = useOutletContext() || {};
