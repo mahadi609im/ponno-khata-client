f941799c4fb8f8fe4d539bd2b24c54a93ff37e59
mahadi609im <maha609im@gmail.com>
2026-08-06 14:18:28 +0600

    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import AllProducts from '../Components/All Products/AllProducts';
 import CategoryProducts from '../Components/CategoryProducts';
 
 const HomePage = () => {
-  // Outlet context থেকে activeCategory রিসিভ করা
+  // Outlet context থেকে activeCaaategory রিসিভ করা
   const context = useOutletContext() || {};
   const { activeCategory } = context;
 
